67262dd483e8ad23475429b454c475ee716dc4c8
ViniciusPrado7 <vinicius.pradofake@gmail.com>
2025-07-01 15:48:22 -0300

    fix vite.config.js and .gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(dirname, 'index.html'),
+        inicio: resolve(dirname, './src/pages/Inicio.jsx'),
+        NaoEncontrada: resolve(dirname, './src/pages/NaoEncontrada.jsx'),
+        post: resolve(dirname, './src/pages/Post.jsx'),
+        sobreMim: resolve(dirname, './src/pages/SobreMim.jsx'),
+      }
+    }
+  }
 })
