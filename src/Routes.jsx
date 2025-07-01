22972d1a556ffd4a723c5647fda46eada434140b
ViniciusPrado7 <vinicius.pradofake@gmail.com>
2025-07-01 15:17:14 -0300

    Fix broken import path for Sobremim component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./pages/Inicio";
-import Sobremim from "./pages/Sobremim";
+import SobreMim from "./pages/SobreMim";
 import Menu from "./componentes/Menu";
 import Rodape from "./componentes/Rodape";
 import PaginaPadrao from "./componentes/PaginaPadrao";
@@ -19,7 +19,7 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
-          <Route path="/sobremim" element={<Sobremim />} />
+          <Route path="/sobremim" element={<SobreMim />} />
         </Route>
         <Route path="/posts/:id/*" element={<Post />} />
         {/* 
